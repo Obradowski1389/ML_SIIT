e880680933bd60304332617b8af3d75b7a347413
Obradowski1389 <isomidobradovic@gmail.com>
2024-03-04 13:58:45 +0100

    repo and zad1 init

---
+++ zad1/main.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+import sys
+
+
+def load_data(path):
+    data = pd.read_csv(path, header=None)
+    return data
+
+
+def plot_data():
+    pass
+
+
+def GD_batch(X, y, learning_rate=0.01, num_iterations=1000):
+    pass
+
+
+def GD_stochastic(X, y, learning_rate=0.01, num_iterations=1000):
+    pass
+
+
+def normal_equation(X, y):
+    pass
+
+
+def train_model(X, y, method, learning_rate=0.01, num_iterations=1000):
+    if method == 'GD_batch':
+        return GD_batch(X, y, learning_rate, num_iterations)
+    elif method == 'GD_stochastic':
+        return GD_stochastic(X, y, learning_rate, num_iterations)
+    elif method == 'normal_equation':
+        return normal_equation(X, y)
+    else:
+        return None
+
+
+def calculate_mse(predictions, targets):
+    return np.mean((predictions - targets) ** 2)
+
+
+def normalize_data(data, method):   # Multiple types of normalization; add more?
+    if method == 'min-max':
+        pass
+    elif method == 'z-score':
+        pass
+    elif method == 'log':
+        pass
+    else:
+        return data
+
+
+def handle_outliers(data, method): # Multiple types of handling outliers
+    if method == 'remove':
+        pass
+    elif method == 'z-score':
+        pass   
+    elif method == 'log':
+        pass
+    elif method == 'mean':
+        pass
+    elif method == 'robust-regression':
+        pass
+    else:
+        return data
+
+
+def main(path):
+    data = load_data(path)
+    X = data['X'].values.reshape(-1, 1)  
+    Y = data['Y'].values
+
+    optimization_algorithms = [GD_batch, GD_stochastic]
+    normalization_methods = ['min-max', 'z-score', 'log']
+    outlier_handling_methods = ['remove', 'z-score', 'log', 'mean', 'robust-regression']
+    results = []
+
+    for opt_algo in optimization_algorithms:
+        for norm_method in normalization_methods:
+            for outlier_method in outlier_handling_methods:
+                
+                normalized_X = normalize_data("", norm_method)
+                cleaned_X = handle_outliers(normalized_X, outlier_method)
+
+                predictions = train_model()
+                mse = calculate_mse(predictions, y)
+
+                results.append({
+                    'Optimization Algorithm': opt_algo.__name__,
+                    'Normalization Method': norm_method,
+                    'Outlier Handling Method': outlier_method,
+                    'MSE': mse
+                })
+
+    
+    results_df = pd.DataFrame(results)
+    sorted_results_df = results_df.sort_values(by='MSE', ascending=True) 
+    sorted_results_df.to_csv("results.csv", index=False)
+
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    main(path)
