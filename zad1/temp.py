f387cf50f8d84ec7f4b12bdbe5dfca74a5ba6c76
Obradowski1389 <isomidobradovic@gmail.com>
2024-03-19 12:34:59 +0100

    Finised zad1 with 175 rmse

--- zad1/temp.py
+++ zad1/temp.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import sys
 
 
 def train_test_split(X, y, test_size=0.2, shuffle=True):
@@ -21,19 +21,19 @@ def train_test_split(X, y, test_size=0.2, shuffle=True):
     return X_train, X_test, y_train, y_test
 
 
-def linear_regression_fit(X_train, y_train):
-    X_train = np.column_stack((np.ones(len(X_train)), X_train))
-    coefficients = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
-    
-    return coefficients
-
-
 # Z-score
 def normalize(X):
     mean = np.mean(X, axis=0)
     std_dev = np.std(X, axis=0)
     X_normalized = (X - mean) / std_dev
     return X_normalized, mean, std_dev
+
+
+def linear_regression_fit(X_train, y_train):
+    X_train = np.column_stack((np.ones(len(X_train)), X_train))
+    coefficients = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
+    
+    return coefficients
 
 
 def linear_regression_predict(X, coefficients):
@@ -43,55 +43,37 @@ def linear_regression_predict(X, coefficients):
     return predictions
 
 
-def root_mean_squared_error(y_true, y_pred):
-    return np.sqrt(np.mean((y_pred - y_true )**2))
+def calculate_rmse(y_true, y_pred):
+    residuals = y_true - y_pred
+    squared_residuals = residuals ** 2
+    mean_squared_residuals = squared_residuals.mean()
+    rmse = np.sqrt(mean_squared_residuals)
+    return rmse
 
 
-def plot_data(X, Y):
-    fig, ax = plt.subplots(figsize=(6, 4))
-    ax.scatter(X, Y)
-    plt.show()
+def main(path_train, path_test):
+    data = pd.read_csv(path_train)
+    X_train = data['X']
+    y_train = data['Y']
 
-def plot_data_with_regression_line(X, y, coefficients):
-    fig, ax = plt.subplots(figsize=(8, 6))
-    ax.scatter(X, y, label='Data')
-    
-    # Predict Y values using the linear regression model
-    X_regression = np.linspace(np.min(X), np.max(X), 100)
-    y_regression = linear_regression_predict(X_regression, coefficients)
-    
-    # Plot the regression line
-    ax.plot(X_regression, y_regression, color='red', label='Linear Regression')
-    
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.legend()
-    plt.show()
+    testing = pd.read_csv(path_test)
+    X_test = testing['X']
+    y_test = testing['Y']
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
+    X_train_normalized, mean_train, std_dev_train = normalize(X_train)
+    X_test_normalized = (X_test - mean_train) / std_dev_train   # Normalize test data
 
-def main():
-    data = pd.read_csv('data/train.csv')
-    X = data['X']
-    y = data['Y']
+    coefficients = linear_regression_fit(X_train_normalized, y_train)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+    # train_predictions = linear_regression_predict(X_train, coefficients)
+    test_predictions = linear_regression_predict(X_test_normalized, coefficients)
 
-    # X_train_normalized, mean_train, std_dev_train = normalize(X_train)
-    # X_test_normalized = (X_test - mean_train) / std_dev_train   # Normalize test data
+    # train_rmse = calculate_rmse(y_train, train_predictions)
+    test_rmse = calculate_rmse(y_test, test_predictions)
 
-    coefficients = linear_regression_fit(X_train, y_train)
-
-    train_predictions = linear_regression_predict(X_train, coefficients)
-    test_predictions = linear_regression_predict(X_test, coefficients)
-
-    train_rmse = root_mean_squared_error(y_train, train_predictions)
-    test_rmse = root_mean_squared_error(y_test, test_predictions)
-
-    print("Train Predictions:", train_rmse)
-    print("Test Predictions:", test_rmse)
-
-    plot_data_with_regression_line(X_train, y_train, coefficients)
+    print(test_rmse)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
