210730ad0e3bb0c0c5ddafe6926611e5d8f6ba66
Danica Gazdić <danicagazdic@gmail.com>
2024-03-31 23:00:03 +0200

    Implementation with rmse 20000

--- zad2/main.py
+++ zad2/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import scipy
-import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sys
+from sklearn import linear_model
 
 def visualize_correlation_matrix(correlation_matrix):
     # Create a heatmap using Seaborn
@@ -12,20 +12,22 @@ def visualize_correlation_matrix(correlation_matrix):
     plt.title('Correlation Matrix')
     plt.show()
 
-
 def label_encode(train_data, test_data):
     label_mappings = {}
     
     categorical_columns = ['Marka', 'Grad', 'Karoserija', 'Gorivo', 'Menjac']
     for column in categorical_columns:
-        categories = pd.concat([train_data[column], test_data[column]]).unique()
+        categories = sorted(pd.concat([train_data[column], test_data[column]]).unique())
+    
         label_map = {category: i for i, category in enumerate(categories)}
         label_mappings[column] = label_map
+        
         train_data[column] = train_data[column].map(label_map)
         test_data[column] = test_data[column].map(label_map)
     
-    return train_data, test_data, 
-    
+    return train_data, test_data
+
+
 
 def calculate_rmse(y_true, y_pred):
     residuals = y_true - y_pred
@@ -34,22 +36,68 @@ def calculate_rmse(y_true, y_pred):
     rmse = np.sqrt(mean_squared_residuals)
     return rmse
 
+def predict(X, w, bias):
+    return np.dot(X, w) + bias
+
+def normalize_features(X_train, X_test):
+    # z-score normalization
+    mean = X_train.mean()
+    std = X_train.std()
+    X_train_normalized = (X_train - mean) / std
+    X_test_normalized = (X_test - mean) / std
+    return X_train_normalized, X_test_normalized
+
+def fit_with_regularization(X, y, learning_rate=0.0001, max_iterations=10000, lambda_=0.01):
+    weights = np.zeros(X.shape[1])
+    bias = 0
+    loss = []
+
+    for _ in range(max_iterations):
+        y_hat = np.dot(X, weights) + bias
+        loss.append(calculate_rmse(y, y_hat))
+
+        partial_w = (1 / X.shape[0]) * (2 * np.dot(X.T, (y_hat - y)))
+        partial_b = (1 / X.shape[0]) * (2 * np.sum(y_hat - y))
+
+        # L1 regularization
+        regularization_term = lambda_ * weights
+        partial_w += regularization_term
+
+        weights -= learning_rate * partial_w
+        bias -= learning_rate * partial_b
+
+    return weights, bias, loss
+
 
 def main(train_path, test_path):
     train_data = pd.read_csv(train_path, sep='\t')
     test_data = pd.read_csv(test_path, sep='\t')
     train_data, test_data = label_encode(train_data, test_data)
+
+    train_data['Cena'] = np.log1p(train_data['Cena'])
+    test_data['Cena'] = np.log1p(test_data['Cena'])
     
-    correlation_matrix = train_data.corr()  
-    # visualize_correlation_matrix(correlation_matrix)
-
-    # TODO drop more columns based of correlation matrix
     y_train = train_data['Cena']
-    X_train = train_data.drop(columns=['Cena'])
+    X_train = train_data.drop(columns=['Cena', 'Grad', 'Gorivo'])
     y_test = test_data['Cena']
-    X_test = test_data.drop(columns=['Cena'])
+    X_test = test_data.drop(columns=['Cena', 'Grad', 'Gorivo'])
 
+    # Feature scaling
+    X_train_normalized, X_test_normalized = normalize_features(X_train, X_test)
 
+    # Fit with regularization
+    w, b, loss = fit_with_regularization(X_train_normalized, y_train, lambda_=1)
+
+    # Prediction
+    y_pred = predict(X_test_normalized, w, b)
+
+    y_test = np.expm1(y_test)
+    y_pred = np.expm1(y_pred)
+    print(y_pred)
+    rmse = calculate_rmse(y_test, y_pred)
+    print(rmse)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
+
+
