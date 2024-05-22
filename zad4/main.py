81dd92285078543dbe9bd59185abdc1095ab4f0e
Obradowski1389 <isomidobradovic@gmail.com>
2024-05-22 08:31:07 +0200

    Finished with 0.432 on platform

--- zad4/main.py
+++ zad4/main.py
@@ -1,6 +1,6 @@
+import sys
 import pandas as pd
 from sklearn.impute import KNNImputer
-from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
 from sklearn.metrics import f1_score
@@ -14,8 +14,10 @@ def main(train, test):
     train_data = pd.read_csv(train)
     test_data = pd.read_csv(test)
 
+
     imputer = KNNImputer()
     train_data[numerical_features] = imputer.fit_transform(train_data[numerical_features])
+
 
     encoder = OneHotEncoder()
     platform_encoded = encoder.fit_transform(train_data[categorical_features])
@@ -23,49 +25,41 @@ def main(train, test):
 
     train_data_final = pd.concat([train_data[numerical_features], platform_encoded_df, train_data['Genre']], axis=1)
 
-    # Podela na trening i validacioni skup
+
     y = train_data_final['Genre']
     X = train_data_final.drop('Genre', axis=1)
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Treniranje ansambl klasifikatora
-    rf = RandomForestClassifier(random_state=42)
-    gb = GradientBoostingClassifier(random_state=42)
+    # ansambl
+    rf = RandomForestClassifier(
+        n_estimators=50,
+        max_depth=20,
+        min_samples_split=7,
+        random_state=42
+    )
+    gb = GradientBoostingClassifier(
+        n_estimators=100,
+        learning_rate=0.3,
+        random_state=42
+    )
 
     voting_clf = VotingClassifier(estimators=[('rf', rf), ('gb', gb)], voting='soft')
+    voting_clf.fit(X, y)
 
-    # Podesavanje hiperparametara
-    param_grid = {
-        'rf__n_estimators': [100, 200],
-        'gb__n_estimators': [100, 200]
-    }
-    grid_search = GridSearchCV(voting_clf, param_grid, cv=5, scoring='f1_macro')
-    grid_search.fit(X_train, y_train)
 
-    best_model = grid_search.best_estimator_
+    y_test = test_data['Genre']
 
-    # Evaluacija modela
-    y_val_pred = best_model.predict(X_val)
-    macro_f1 = f1_score(y_val, y_val_pred, average='macro')
-    print(f'Macro F1 Score on Validation Set: {macro_f1}')
-
-    # Zakomentarisati kada se deploy-uje model na platformi
-    test_data[numerical_features] = imputer.transform(test_data[numerical_features])
+    # Zakomentarisati liniju ispod kada se deploy-uje model na platformi
+    # test_data[numerical_features] = imputer.transform(test_data[numerical_features])
     platform_encoded_test = encoder.transform(test_data[categorical_features])
     platform_encoded_test_df = pd.DataFrame(platform_encoded_test.toarray(), columns=encoder.get_feature_names_out(categorical_features))
 
-    test_data_final = pd.concat([test_data[numerical_features], platform_encoded_test_df], axis=1)
+    test_data = pd.concat([test_data[numerical_features], platform_encoded_test_df], axis=1)
 
-    # Predikcija na testnom skupu
-    y_test = test_data['Genre']
-    X_test = test_data_final
-
-    test_predictions = best_model.predict(X_test)
+    test_predictions = voting_clf.predict(test_data)
 
     macro_f1_test = f1_score(y_test, test_predictions, average='macro')
-    print(f'Macro F1 Score on Test Set: {macro_f1_test}')
-
+    print(macro_f1_test)
 
 
 if __name__ == '__main__':
-    main('train.csv', 'test.csv')
+    main(sys.argv[1], sys.argv[2])
